392f21137eccb713134cbbca7a242c859813fa61
withoutemot1on <99506940+withoutemot1on@users.noreply.github.com>
2023-01-12 14:13:57 +0200

    Add files via upload

---
+++ vue23-main/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import './theme.css'
+import { store } from './components/store' 
+
+const news = store()
+
+
+
+
+</script>
+
+<template>
+  <date-header />
+  <news-header v-for="elem in news" :key="elem" :newsTitle="elem.h1" :newsText="elem.text" />
+  
+</template>
---
+++ vue23-main/src/components/NewsHeader.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue'
+
+let count = ref(0);
+let isOpen = ref(false)
+let isRead = ref(false)
+let read = ref(0)
+
+defineProps({
+  newsTitle: String,
+  newsText: String
+})
+
+function btnHandler(){
+  if(!this.isOpen) this.count += 1 
+  this.isOpen = !this.isOpen
+}
+function readPost(){
+  if(!this.isRead) this.read += 1 
+  else this.read  -= 1
+  this.isRead = !this.isRead 
+}
+
+</script>
+
+<template>
+  <div>
+    <h3>{{ newsTitle }}</h3>
+    <button class="btn" @click="btnHandler()">{{isOpen ? 'Свернуть' : 'Развернуть'}}</button>
+    <p v-if="isOpen">{{ newsText }}</p>
+    <p>Просмотров статьи: {{ count }}</p>
+    <button class="btn" @click="readPost()">{{isRead ? 'Непрочитано' : 'Прочитано'}}</button>
+    <p v-if="isRead"></p>
+    <p>Статья прочитана: {{read}} раз</p>
+  </div>
+</template>
+
+<style scoped lang="sass">
+
+</style>
