f77725752a97e3602475660a825ebadd7687c275
withoutemot1on <99506940+withoutemot1on@users.noreply.github.com>
2023-01-13 17:01:00 +0200

    Delete vue23-main directory

--- vue23-main/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import NewsHeader from './components/NewsHeader.vue'
-import DateHeader from './components/DateHeader.vue'
-
-createApp(App)
-.component('news-header', NewsHeader)
-.component('date-header', DateHeader)
-.mount('#app')
