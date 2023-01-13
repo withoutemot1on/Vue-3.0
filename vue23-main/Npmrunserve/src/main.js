091f4c8c945787d10f5d61726b36a7088a970237
withoutemot1on <99506940+withoutemot1on@users.noreply.github.com>
2023-01-13 17:00:03 +0200

    Delete vue23-main/Npmrunserve directory

--- vue23-main/Npmrunserve/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './style.css'
-import theHeader from './theHeader.vue'
-const app = createApp(App)
-app.component('the-header',theHeader)
-app.mount('#app')
